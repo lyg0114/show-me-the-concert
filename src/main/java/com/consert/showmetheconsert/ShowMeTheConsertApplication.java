f70fed203f9caef9833b5655616ad09b84357617
lyg0114 <whdnseowkd@naver.com>
2023-04-30 15:29:46 +0900

    add : chromDriver

--- src/main/java/com/consert/showmetheconsert/ShowMeTheConsertApplication.java
+++ src/main/java/com/consert/showmetheconsert/ShowMeTheConsertApplication.java
@@ -1,13 +1,29 @@
 package com.consert.showmetheconsert;
 
+import com.consert.showmetheconsert.conf.GlobalVar;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ShowMeTheConsertApplication {
 
   public static void main(String[] args) {
+    System.setProperty(GlobalVar.DRIVER_NAME, GlobalVar.DRIVER_PATH);
     SpringApplication.run(ShowMeTheConsertApplication.class, args);
   }
 
+  @Bean
+  public WebDriver webDriver() {
+    return new ChromeDriver(chromeOptions());
+  }
+
+  @Bean
+  public ChromeOptions chromeOptions() {
+    return new ChromeOptions()
+        .addArguments("--remote-allow-origins=*");
+  }
 }
---
+++ src/main/java/com/consert/showmetheconsert/conf/GlobalVar.java
@@ -0,0 +1,21 @@
+package com.consert.showmetheconsert.conf;
+
+import java.time.Duration;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.consert.showmetheconsert.conf
+ * @since : 2023/04/30
+ */
+@Getter
+@Component
+public class GlobalVar {
+
+  public static Duration COMMON_DELAY = Duration.ofMillis(10);
+  public static String DRIVER_NAME = "webdriver.chrome.driver";
+  public static String RESOURCE_PATH = "/Users/iyeong-gyo/Desktop/study/cicd-study/show-me-the-consert/src/main/resources";
+  public static String DRIVER_PATH = RESOURCE_PATH + "/chromedriver";
+}
