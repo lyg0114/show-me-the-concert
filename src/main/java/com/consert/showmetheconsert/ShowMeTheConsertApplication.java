6727185f3a005fb3055d6386346fd4a87a36ecdb
lyg0114 <whdnseowkd@naver.com>
2023-04-30 15:16:58 +0900

    init project

---
+++ src/main/java/com/consert/showmetheconsert/ShowMeTheConsertApplication.java
@@ -0,0 +1,13 @@
+package com.consert.showmetheconsert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowMeTheConsertApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShowMeTheConsertApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/consert/showmetheconsert/ShowMeTheConsertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.consert.showmetheconsert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowMeTheConsertApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
