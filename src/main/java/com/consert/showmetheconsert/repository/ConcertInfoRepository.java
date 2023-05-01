8f5601fe82af61c90f6d33e6794552255fff879c
lyg0114 <whdnseowkd@naver.com>
2023-05-01 21:17:03 +0900

    Add : ConcertInfoRepository

---
+++ src/main/java/com/consert/showmetheconsert/repository/ConcertInfoRepository.java
@@ -0,0 +1,12 @@
+package com.consert.showmetheconsert.repository;
+
+import com.consert.showmetheconsert.model.entity.ConcertInfo;
+import org.springframework.data.repository.Repository;
+
+/**
+ * @author : iyeong-gyo
+ * @package : com.consert.showmetheconsert.repository
+ * @since : 2023/05/01
+ */
+public interface ConcertInfoRepository extends Repository<ConcertInfo, Long> {
+}
