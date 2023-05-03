574e78e042779ae8e835cb84b1e30203205d703c
lyg0114 <whdnseowkd@naver.com>
2023-05-03 20:20:35 +0900

    Fix : change extends repository to JpaRepository

--- src/main/java/com/consert/showmetheconsert/repository/ConcertInfoRepository.java
+++ src/main/java/com/consert/showmetheconsert/repository/ConcertInfoRepository.java
@@ -1,12 +1,15 @@
 package com.consert.showmetheconsert.repository;
 
 import com.consert.showmetheconsert.model.entity.ConcertInfo;
-import org.springframework.data.repository.CrudRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * @author : iyeong-gyo
  * @package : com.consert.showmetheconsert.repository
  * @since : 2023/05/01
  */
-public interface ConcertInfoRepository extends CrudRepository<ConcertInfo, Long> {
+public interface ConcertInfoRepository extends JpaRepository<ConcertInfo, Long> {
+  List<ConcertInfo> findByConcertDateTimeBetween(LocalDateTime start, LocalDateTime end);
 }
