c2bde2189e9abebee491a1fb8405083eb4d7c650
lyg0114 <whdnseowkd@naver.com>
2023-05-05 17:02:48 +0900

    Fix : change class name

--- src/main/java/com/consert/showmetheconsert/service/MakeCalendarService.java
+++ src/main/java/com/consert/showmetheconsert/service/MakeCalendarService.java
@@ -2,8 +2,8 @@ package com.consert.showmetheconsert.service;
 
 import com.consert.showmetheconsert.model.entity.ConcertInfo;
 import com.consert.showmetheconsert.repository.ConcertInfoRepository;
-import com.consert.showmetheconsert.model.dto.CalendarDay;
-import com.consert.showmetheconsert.model.dto.CalendarSlot;
+import com.consert.showmetheconsert.model.dto.CalendarDayDto;
+import com.consert.showmetheconsert.model.dto.CalendarSlotDto;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -34,12 +34,12 @@ public class MakeCalendarService {
     return daysOfWeek;
   }
 
-  public List<List<CalendarDay>> makeCalendar() {
+  public List<List<CalendarDayDto>> makeCalendar() {
     LocalDateTime start = LocalDateTime.of(2023, 5, 1, 0, 0);
     LocalDateTime end = LocalDateTime.of(2023, 6, 1, 0, 0);
     List<ConcertInfo> concertInfos = concertInfoRepo.findByConcertDateTimeBetween(start, end);
 
-    List<List<CalendarDay>> calendarRows = new ArrayList<>();
+    List<List<CalendarDayDto>> calendarRows = new ArrayList<>();
     makeBasicCalendar(calendarRows);
     asignConcertInfos(concertInfos, calendarRows);
     cleanTable(calendarRows);
@@ -47,21 +47,21 @@ public class MakeCalendarService {
     return calendarRows;
   }
 
-  private void makeBasicCalendar(List<List<CalendarDay>> calendarRows) {
+  private void makeBasicCalendar(List<List<CalendarDayDto>> calendarRows) {
     LocalDate startDate = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
     LocalDate endDate = startDate.plusMonths(1).minusDays(1);
     LocalDate currentDate = startDate;
     while (!currentDate.isAfter(endDate)) {
-      List<CalendarDay> row = new ArrayList<>();
+      List<CalendarDayDto> row = new ArrayList<>();
       for (int i = 0; i < 7 && !currentDate.isAfter(endDate); i++) {
-        CalendarDay day = new CalendarDay(currentDate);
-        List<CalendarSlot> slots = new ArrayList<>();
+        CalendarDayDto day = new CalendarDayDto(currentDate);
+        List<CalendarSlotDto> slots = new ArrayList<>();
         for (int j = 0; j <= 23; j++) {
           for (int k = 0; k < 2; k++) {
             LocalTime startTime = LocalTime.of(j, k * 30);
             LocalDateTime currentConcertDateTime = LocalDateTime.of(currentDate, startTime);
             LocalTime endTime = startTime.plusMinutes(30);
-            CalendarSlot slot = new CalendarSlot(currentConcertDateTime, startTime, endTime);
+            CalendarSlotDto slot = new CalendarSlotDto(currentConcertDateTime, startTime, endTime);
             slots.add(slot);
           }
         }
@@ -73,10 +73,10 @@ public class MakeCalendarService {
     }
   }
 
-  private void asignConcertInfos(List<ConcertInfo> concertInfos, List<List<CalendarDay>> calendarRows) {
-    for (List<CalendarDay> calendarRow : calendarRows) {
-      for (CalendarDay calendarDay : calendarRow) {
-        for (CalendarSlot slot : calendarDay.getSlots()) {
+  private void asignConcertInfos(List<ConcertInfo> concertInfos, List<List<CalendarDayDto>> calendarRows) {
+    for (List<CalendarDayDto> calendarRow : calendarRows) {
+      for (CalendarDayDto calendarDay : calendarRow) {
+        for (CalendarSlotDto slot : calendarDay.getSlots()) {
           LocalDateTime currentConcertDateTime = slot.getCurrentConcertDateTime();
           concertInfos.stream()
               .filter(
@@ -87,9 +87,9 @@ public class MakeCalendarService {
     }
   }
 
-  private void cleanTable(List<List<CalendarDay>> calendarRows) {
-    for (List<CalendarDay> calendarRow : calendarRows) {
-      for (CalendarDay calendarDay : calendarRow) {
+  private void cleanTable(List<List<CalendarDayDto>> calendarRows) {
+    for (List<CalendarDayDto> calendarRow : calendarRows) {
+      for (CalendarDayDto calendarDay : calendarRow) {
         calendarDay.getSlots().removeIf(slot -> slot.getConcertInfos().isEmpty());
       }
     }
