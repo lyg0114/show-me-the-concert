15e35d15a64af1cae5a72bb7b0fdbe7b3c8151ee
lyg0114 <whdnseowkd@naver.com>
2023-05-07 20:49:44 +0900

    Add test : testCalculateConcertDate()

--- src/test/java/com/consert/showmetheconsert/util/TimeUtilTest.java
+++ src/test/java/com/consert/showmetheconsert/util/TimeUtilTest.java
@@ -1,16 +1,33 @@
 package com.consert.showmetheconsert.util;
 
+import static org.mockito.Mockito.when;
+
+import com.consert.showmetheconsert.conf.GlobalVar;
+import com.consert.showmetheconsert.repository.ConcertInfoRepository;
+import com.consert.showmetheconsert.schedule.SearchDaeguConcertSchedule;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 /**
  * @author : iyeong-gyo
  * @package : com.consert.showmetheconsert.util
  * @since : 2023/05/07
  */
+@DataJpaTest
 class TimeUtilTest {
+
+  @Autowired
+  private ConcertInfoRepository concertInfoRepo;
+  private SearchDaeguConcertSchedule schedule;
+
+  private SearchDaeguConcertSchedule getSearchDaeguConcertSchedule() {
+    return new SearchDaeguConcertSchedule(
+        new GlobalVar(), new DummyWebDriver(), concertInfoRepo);
+  }
 
   @Test
   public void testExpressionWhenDataParse() {
@@ -23,4 +40,11 @@ class TimeUtilTest {
       Assertions.assertEquals("2023-05-07 19:00", datetimeStr);
     }
   }
+
+  @Test
+  public void testCalculateConcertDate() {
+    SearchDaeguConcertSchedule schedule = getSearchDaeguConcertSchedule();
+    when(null).thenReturn(null);
+  }
+
 }
