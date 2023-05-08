b40e327d37f690ef48da0eb42279042a7b75219f
lyg0114 <whdnseowkd@naver.com>
2023-05-09 00:23:28 +0900

    Add : refactoring test code, TimeUtilTest class

--- src/test/java/com/consert/showmetheconsert/util/TimeUtilTest.java
+++ src/test/java/com/consert/showmetheconsert/util/TimeUtilTest.java
@@ -1,13 +1,9 @@
 package com.consert.showmetheconsert.util;
 
-import static org.mockito.Mockito.when;
-
 import com.consert.showmetheconsert.conf.GlobalVar;
 import com.consert.showmetheconsert.repository.ConcertInfoRepository;
 import com.consert.showmetheconsert.schedule.SearchDaeguConcertSchedule;
 import java.time.LocalDateTime;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,18 +24,6 @@ class TimeUtilTest {
   private SearchDaeguConcertSchedule getSearchDaeguConcertSchedule() {
     return new SearchDaeguConcertSchedule(
         new GlobalVar(), new DummyWebDriver(), concertInfoRepo);
-  }
-
-  @Test
-  public void testExpressionWhenDataParse() {
-    String timeStr = "DUMMY_STRING 2023-05-07 19:00";
-    Pattern pattern = Pattern.compile("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}");
-    Matcher matcher = pattern.matcher(timeStr);
-    String datetimeStr = null;
-    if (matcher.find()) {
-      datetimeStr = matcher.group();
-      Assertions.assertEquals("2023-05-07 19:00", datetimeStr);
-    }
   }
 
   @Test
