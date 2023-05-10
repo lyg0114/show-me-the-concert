af18df2ed6eaae646d3a548b738e035d4a32cb0d
lyg0114 <whdnseowkd@naver.com>
2023-05-10 21:29:04 +0900

    Update : change month btn activate

--- src/main/java/com/consert/showmetheconsert/controller/ShowScheduleController.java
+++ src/main/java/com/consert/showmetheconsert/controller/ShowScheduleController.java
@@ -1,6 +1,7 @@
 package com.consert.showmetheconsert.controller;
 
 import com.consert.showmetheconsert.service.MakeCalendarService;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -35,9 +36,19 @@ public class ShowScheduleController {
   public String showSchedule(
       @PathVariable("year") int year,
       @PathVariable("month") int month, Model model) {
-
     model.addAttribute("daysOfWeek", calendarService.getDayOfWeeks(year, month));
     model.addAttribute("calendar", calendarService.makeCalendar(year, month));
+    setBeforeNextInfo(model, year, month);
+
     return VIEWS_CONCERT_SCUEDULE;
+  }
+
+  private void setBeforeNextInfo(Model model, int year, int month) {
+    model.addAttribute("currentYear", LocalDate.of(year, month, 1).getYear());
+    model.addAttribute("currentMonth", LocalDate.of(year, month, 1).getMonthValue());
+    model.addAttribute("beforeYear", LocalDate.of(year, month, 1).minusMonths(1).getYear());
+    model.addAttribute("beforeMonth", LocalDate.of(year, month, 1).minusMonths(1).getMonthValue());
+    model.addAttribute("afterYear", LocalDate.of(year, month, 1).plusMonths(1).getYear());
+    model.addAttribute("afterMonth", LocalDate.of(year, month, 1).plusMonths(1).getMonthValue());
   }
 }
