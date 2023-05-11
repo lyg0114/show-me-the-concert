1a6f20c1d8e4164e7e81fd2ec7620602812ff1df
lyg0114 <whdnseowkd@naver.com>
2023-05-11 20:32:20 +0900

    Add : add Repository annotation

--- src/main/java/com/consert/showmetheconsert/repository/ConcertInfoRepository.java
+++ src/main/java/com/consert/showmetheconsert/repository/ConcertInfoRepository.java
@@ -4,12 +4,14 @@ import com.consert.showmetheconsert.model.entity.ConcertInfo;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author : iyeong-gyo
  * @package : com.consert.showmetheconsert.repository
  * @since : 2023/05/01
  */
+@Repository
 public interface ConcertInfoRepository extends JpaRepository<ConcertInfo, Long> {
   List<ConcertInfo> findByConcertDateTimeBetween(LocalDateTime start, LocalDateTime end);
 }
