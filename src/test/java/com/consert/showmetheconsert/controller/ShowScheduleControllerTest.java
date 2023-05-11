2e8c0e9f118cf351376472fc784e4ace6af8f526
lyg0114 <whdnseowkd@naver.com>
2023-05-11 20:32:56 +0900

    Fix : refactoring ShowScheduleControllerTest

--- src/test/java/com/consert/showmetheconsert/controller/ShowScheduleControllerTest.java
+++ src/test/java/com/consert/showmetheconsert/controller/ShowScheduleControllerTest.java
@@ -6,7 +6,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 /**
@@ -14,7 +15,8 @@ import org.springframework.test.web.servlet.MockMvc;
  * @package : com.consert.showmetheconsert.controller
  * @since : 2023/04/30
  */
-@WebMvcTest(ShowScheduleController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class ShowScheduleControllerTest {
 
   @Autowired
