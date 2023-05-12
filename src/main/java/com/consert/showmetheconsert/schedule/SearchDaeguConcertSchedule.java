b50220dd31be964fd79290c0d9914624aec1fdcb
lyg0114 <whdnseowkd@naver.com>
2023-05-12 23:55:40 +0900

    Update : replac annotation

--- src/main/java/com/consert/showmetheconsert/schedule/SearchDaeguConcertSchedule.java
+++ src/main/java/com/consert/showmetheconsert/schedule/SearchDaeguConcertSchedule.java
@@ -16,7 +16,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -26,7 +26,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Slf4j
 @RequiredArgsConstructor
-@Service
+@Component
 public class SearchDaeguConcertSchedule {
 
   public static final String RETURN_BTN_XPATH = "/html/body/div[2]/div[3]/div/div/div/div[2]/div[1]/ul/li[3]/a";
