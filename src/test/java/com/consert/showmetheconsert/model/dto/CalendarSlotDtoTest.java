7fe42f77b9c97b7bf5ac989f0010e7c92c588803
lyg0114 <whdnseowkd@naver.com>
2023-05-14 16:57:44 +0900

    Add : add test code CalendarSlotDtoTest

--- src/test/java/com/consert/showmetheconsert/model/dto/CalendarSlotDtoTest.java
+++ src/test/java/com/consert/showmetheconsert/model/dto/CalendarSlotDtoTest.java
@@ -1,7 +1,9 @@
 package com.consert.showmetheconsert.model.dto;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.time.LocalTime;
-import org.junit.jupiter.api.Assertions;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,11 +12,18 @@ import org.junit.jupiter.api.Test;
  * @since : 2023/05/14
  */
 class CalendarSlotDtoTest {
-
   @Test
   public void test_LocalTime() {
     LocalTime now = LocalTime.of(00, 00);
     LocalTime now2 = LocalTime.of(00, 00);
-    Assertions.assertEquals(now, now2);
+    assertEquals(now, now2);
+  }
+
+  @Test
+  public void test_LocalTime_to_String() {
+    LocalTime now = LocalTime.of(00, 00);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+    String timeStr = now.format(formatter);
+    assertEquals("00:00", timeStr);
   }
 }
