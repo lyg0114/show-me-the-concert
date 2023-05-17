47837374bb2e1cea1f30119067f52c2000a55826
lyg0114 <whdnseowkd@naver.com>
2023-05-17 19:16:44 +0900

    WORK-8 Impl : add getUnEscapeTitle method

--- src/main/java/com/consert/showmetheconsert/model/entity/ConcertInfo.java
+++ src/main/java/com/consert/showmetheconsert/model/entity/ConcertInfo.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.format.annotation.DateTimeFormat;
 
 /**
@@ -64,5 +65,9 @@ public class ConcertInfo extends BaseEntity {
     if (newInfo.getUrl() != null) { this.url = newInfo.getUrl(); }
     if (newInfo.getConcertDateTime() != null) { this.concertDateTime = newInfo.getConcertDateTime(); }
     if (newInfo.getConcertHallTag() != null) { this.concertHallTag = newInfo.getConcertHallTag(); }
+  }
+
+  public String getUnEscapeTitle(){
+    return StringEscapeUtils.unescapeHtml4(this.title);
   }
 }
